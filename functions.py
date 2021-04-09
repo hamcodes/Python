cac7d64ca6e6744234be9c4da9ed14d1466f19ae
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2021-04-09 12:05:00 -0400

    add functions

---
+++ functions.py
@@ -0,0 +1,30 @@
+# def greeting():
+#   print('hello there my friend')
+#   print('what is your name')
+#   name = input()
+#   print("nice to meet you " + name)
+
+# greeting()
+
+# def greeting(name):
+#   print('Hello there ' + name)
+
+# greeting("ed")
+
+# def multiply_by_10(number):
+#   print(10 * number)
+
+# multiply_by_10(50)
+
+def multiply_by_10(number):
+  return 10 * number
+
+def add(number, by=1):
+  return number + by
+
+added_number = add(10)
+print(added_number)
+
+# muliplied_number = multiply_by_10(20)
+
+# print(muliplied_number)

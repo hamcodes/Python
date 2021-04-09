4c7f2385e6c0a16a66478f594bea2b5dce2cd224
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2021-04-09 11:51:50 -0400

    add while loop

---
+++ loops.py
@@ -0,0 +1,27 @@
+#for loop
+users = ['ed', 'jeremy', 'podric', 'smith']
+
+# my_list = list(range(0,101,5))
+
+# print(my_list)
+
+# for i in range(0, 10):
+#   print('i run ten times')
+
+# for user in users:
+#   print('Hello there user')
+#   print(user)
+
+# name = 'Jeremy'
+
+# for letter in name:
+#   print(letter)
+
+#while loop
+age = 0
+while age <= 20:
+  print('i am growing up')
+  age += 1
+  if(age == 14):
+    print('i am independant now')
+    continue

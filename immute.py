ee4125cedcfe65ee055d445880cabd016b526db3
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2021-04-09 11:55:46 -0400

    add immute

---
+++ immute.py
@@ -0,0 +1,19 @@
+# fruit = "Apple"
+
+# other_fruit = fruit
+
+# other_fruit = "banana"
+
+# print(other_fruit)
+
+# fruits = ['apple', 'banana', 'kiwi']
+
+# other_fruits = fruits.copy()
+
+# other_fruits.append('Dragon Fruit')
+
+# print(fruits)
+
+#bool int floats tuples strings
+
+#lists, sets, dict -> always keep a reference
